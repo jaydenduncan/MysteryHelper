74daf78f3542d90ad2bdaaa6dc48f404808f96d9
Jayden <jduncan9@stu.jsu.edu>
2023-01-30 18:10:39 -0600

    String Array added, help button click event implemented

--- app/src/main/java/com/example/mysteryhelper/MainActivity.java
+++ app/src/main/java/com/example/mysteryhelper/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.mysteryhelper.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private String[] plots;
+    Button helpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,34 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        plots = getResources().getStringArray(R.array.plot_fragments);
+
+        helpBtn = binding.helpButton;
+
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int randNum = chooseRandNum();
+
+                String plotFragment = plots[randNum];
+
+                binding.plotText.setText(plotFragment);
+
+            }
+        });
+    }
+
+
+
+    private static int chooseRandNum(){
+
+        int result = 0;
+
+        result = (int)(Math.random() * 18);
+
+        return result;
+
     }
 }
